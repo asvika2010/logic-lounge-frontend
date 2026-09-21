1ae996b31f307a78f974f7344dc35407896c9462
Rajkumar <rajkumar@example.com>
2026-09-21 11:22:33 +0530

    Bypass git case-caching by renaming components to modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Grade1Module from "./components/grade1/Grade1Module.jsx";
-import Grade2Module from "./components/grade2/Grade2Module.jsx";
-import Grade3Module from "./components/grade3/Grade3Module.jsx";
+import Grade1Module from "./modules/grade1/Grade1Module.jsx";
+import Grade2Module from "./modules/grade2/Grade2Module.jsx";
+import Grade3Module from "./modules/grade3/Grade3Module.jsx";
 import './App.css';
 
 function App() {
